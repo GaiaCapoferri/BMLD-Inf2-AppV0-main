d78fd313ed10b16c0274ec69827fece790eed8a8
GaiaCapoferri <capofgai@students.zhaw.ch>
2025-02-28 10:45:23 +0100

    Code Rechner 1

--- pages/1_Unterseite A.py
+++ pages/1_Unterseite A.py
@@ -4,3 +4,12 @@ st.title("BMI-Rechner")
 
 st.write("Dieser Rechner berechnet den BMi einer Person und bestimmt anhand einer Tabelle, ob die Person normalgewichtig ist oder nicht.")
 
+height = st.number_input("Geben Sie Ihre Größe in Metern ein:", min_value=0.0, format="%.2f")
+
+weight = st.number_input("Geben Sie Ihr Gewicht in Kilogramm ein:", min_value=0.0, format="%.1f")
+
+if height > 0:
+    bmi = weight / (height ** 2)
+    st.write(f"Ihr BMI beträgt: {bmi:.2f}")
+else:
+    st.write("Bitte geben Sie eine gültige Größe ein.")
