17221e1135592edd44cfe2625fb4a81ae60b8a78
GaiaCapoferri <capofgai@students.zhaw.ch>
2025-03-06 11:31:29 +0100

    übung Vorlesung 3

--- pages/2_Unterseite B.py
+++ pages/2_Unterseite B.py
@@ -3,4 +3,20 @@ import streamlit as st
 st.title("Unterseite B")
 
 st.write("Diese Seite ist eine Unterseite der Startseite.")
+st.write("übung Vorlesung 3.")
+
+# Zähler für die Anzahl der Klicks
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+# Funktion zum Erhöhen des Zählers
+def increment_counter():
+    st.session_state.count += 1
+
+# Knopf zum Erhöhen des Zählers
+if st.button("Klick mich!"):
+    increment_counter()
+
+# Anzeige der Anzahl der Klicks
+st.write(f"Der Knopf wurde {st.session_state.count} mal gedrückt.")
 
